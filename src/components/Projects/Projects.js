7fd0d467e42e39568c995272764155a838f5fa6a
KingCosmic <KingCosmicDev@gmail.com>
2023-02-27 23:03:33 -0600

    added updated Resume

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -5,9 +5,10 @@ import Particle from "../Particle";
 import leaf from "../../Assets/Projects/leaf.png";
 import emotion from "../../Assets/Projects/emotion.png";
 import editor from "../../Assets/Projects/codeEditor.png";
-import chatify from "../../Assets/Projects/chatify.png";
 import suicide from "../../Assets/Projects/suicide.png";
-import bitsOfCode from "../../Assets/Projects/blog.png";
+
+import restaurant from "../../Assets/Projects/restaurant.png";
+import admindashboard from "../../Assets/Projects/admindashboard.png";
 
 function Projects() {
   return (
@@ -23,22 +24,22 @@ function Projects() {
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={chatify}
+              imgPath={restaurant}
               isBlog={false}
-              title="Chatify"
-              description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
-              ghLink="https://github.com/soumyajit4419/Chatify"
-              demoLink="https://chatify-49.web.app/"
+              title="GERICHT"
+              description="Restaurant landing page show ui/ux built with react.js, Material-UI, and Firebase. List Features (TODO)."
+              ghLink="https://github.com/KingCosmic/restaurant"
+              demoLink="https://restaurant-kingcosmic.vercel.app/"
             />
           </Col>
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={bitsOfCode}
+              imgPath={admindashboard}
               isBlog={false}
-              title="Bits-0f-C0de"
-              description="My personal blog page build with Next.js and Tailwind Css which takes the content from makdown files and renders it using Next.js. Supports dark mode and easy to write blogs using markdown."
-              ghLink="https://github.com/soumyajit4419/Bits-0f-C0de"
+              title="Admin Dashboard"
+              description="Admin Dashboard built using the MERN Stack."
+              ghLink="https://github.com/KingCosmic/admin-dashboard"
               demoLink="https://blogs.soumya-jit.tech/"
             />
           </Col>
@@ -50,7 +51,7 @@ function Projects() {
               title="Editor.io"
               description="Online code and markdown editor build with react.js. Online Editor which supports html, css, and js code with instant view of website. Online markdown editor for building README file which supports GFM, Custom Html tags with toolbar and instant preview.Both the editor supports auto save of work using Local Storage"
               ghLink="https://github.com/soumyajit4419/Editor.io"
-              demoLink="https://editor.soumya-jit.tech/"              
+              demoLink="https://codepen.io/KingCosmic"              
             />
           </Col>
 
